9e3085e6cd175dbceb49ae76f68bf9b7eb6ff8e3
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-08-11 00:09:26 +0530

    :art: added css styling for Banner component

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -22,6 +23,12 @@ function Banner() {
   }, []);
 
   console.log(movie, "movie");
+
+  /*This function is used to truncate the length of the description*/
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -33,14 +40,18 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner_content">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
